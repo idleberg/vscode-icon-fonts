adaf6d94f6da1de9277e71022c4e357c0c4333d8
Jan T. Sott <jan@idleberg.com>
2017-10-10 22:23:38 +0200

    user gulp-raster over gulp-svg2png

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,8 @@
 const gulp = require('gulp');
 const debug = require('gulp-debug');
 const jsonlint = require('gulp-jsonlint');
-const svg2png = require('gulp-svg2png');
+const raster = require('gulp-raster');
+const rename = require('gulp-rename');
 const xmlVal = require('gulp-xml-validator');
 
 // Supported files
@@ -27,7 +28,7 @@ const svgFiles = [
 ];
 
 // Lint JSON
-gulp.task('lint:json', gulp.series( (done) => { 
+gulp.task('lint:json', gulp.series( (done) => {
   gulp.src(jsonFiles)
     .pipe(debug({title: 'json-lint'}))
     .pipe(jsonlint())
@@ -37,7 +38,7 @@ gulp.task('lint:json', gulp.series( (done) => {
 }));
 
 // Validate XML
-gulp.task('lint:xml', gulp.series( (done) => { 
+gulp.task('lint:xml', gulp.series( (done) => {
   gulp.src(xmlFiles)
     .pipe(debug({title: 'xml-validator'}))
     .pipe(xmlVal());
@@ -45,9 +46,10 @@ gulp.task('lint:xml', gulp.series( (done) => {
 }));
 
 // Convert SVG
-gulp.task('convert:svg', gulp.series( (done) => { 
+gulp.task('convert:svg', gulp.series( (done) => {
   gulp.src(svgFiles)
-    .pipe(svg2png({width: 128, height: 128}))
+    .pipe(raster())
+    .pipe(rename("logo.png"))
     .pipe(gulp.dest('./images'));
   done();
 }));
